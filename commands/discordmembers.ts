064b268b741d851f769160d7d0d389177f349b46
Stemmi20 <stemmlermoritz300@gmail.com>
2025-04-28 10:00:06 +0000

    Discord Member Cound on every server the bot is on

--- commands/discordmembers.ts
+++ commands/discordmembers.ts
@@ -12,35 +12,6 @@ const tempData: {
   data: { approximate_member_count: 0, approximate_presence_count: 0 },
 };
 
-export const load = async () => {
-  const isTooOld = tempData.lastFetch < Date.now() - 1000;
-  if (isTooOld) {
-    tempData.data = (await fetchDCMembers()) as typeof tempData.data;
-    tempData.lastFetch = Date.now();
-  }
-  return { data: tempData.data };
-};
-
-async function fetchDCMembers() {
-  const url = "https://discord.com/api/v10/invites/SR75j8RB3u?with_counts=true";
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Error fetching WRC quote: ${response.statusText}`);
-    }
-    const data: {
-      approximate_member_count: number;
-      approximate_presence_count: number;
-    } = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
-
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("discordmembers")
@@ -48,10 +19,9 @@ module.exports = {
       "Shows how many People are on the Server and how many online!"
     ),
   async execute(interaction: ChatInputCommandInteraction) {
-    const { data } = await load();
+    const current = await interaction.guild?.fetch();
     await interaction.reply({
-      content: `Members online: ${data.approximate_presence_count} of ${data.approximate_member_count}.`,
-      ephemeral: true,
+      content: `Members online: ${current?.approximatePresenceCount} of ${current?.approximateMemberCount}.`,
     });
   },
 };
